c938a62c07d4ab393ef80e9d208ea0af5559981d
TewobestaT <tewobesta.tilahun@gmail.com>
2022-06-08 04:15:20 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("\_putchar\n");
+return (0);
+}
